c5668af4fd020afc1b0e00f6214a20adbac5cd47
TIHan <lol.tihan@gmail.com>
2013-08-20 16:06:21 -0500

    Android wired up.

--- MvvmCross.CSharp.XS.Bootstrap/src/Bootstrap.Droid/Resources/Resource.designer.cs
+++ MvvmCross.CSharp.XS.Bootstrap/src/Bootstrap.Droid/Resources/Resource.designer.cs
@@ -79,7 +79,13 @@ namespace Bootstrap.Droid
 			public const int Click = 2130968577;
 			
 			// aapt resource value: 0x7f040004
-			public const int GoBack = 2130968580;
+			public const int DisplayText = 2130968580;
+			
+			// aapt resource value: 0x7f040003
+			public const int EnterText = 2130968579;
+			
+			// aapt resource value: 0x7f040006
+			public const int GoBack = 2130968582;
 			
 			// aapt resource value: 0x7f040002
 			public const int GoToNavigate = 2130968578;
@@ -87,8 +93,8 @@ namespace Bootstrap.Droid
 			// aapt resource value: 0x7f040000
 			public const int MvxBindingTagUnique = 2130968576;
 			
-			// aapt resource value: 0x7f040003
-			public const int SendMessage = 2130968579;
+			// aapt resource value: 0x7f040005
+			public const int SendMessage = 2130968581;
 			
 			static Id()
 			{
