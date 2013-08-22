8406ed13bfa8c54523faec0cdbc7a76c19d5eb7f
TIHan <lol.tihan@gmail.com>
2013-08-22 17:46:10 -0500

    Using a SimpleAgent to maintain state.

--- MvvmCross.FSharp.XS.Bootstrap/src/Bootstrap.Core/ViewModels.fs
+++ MvvmCross.FSharp.XS.Bootstrap/src/Bootstrap.Core/ViewModels.fs
@@ -4,22 +4,68 @@ open System
 open System.Collections.ObjectModel
 open Cirrious.MvvmCross.ViewModels
 
+type SimpleAgent<'State, 'Msg> (initial: 'State, execute) =
+    let mailbox = new MailboxProcessor<'Msg> (fun agent ->
+        let rec loop (state: 'State)  =
+            async {
+                let! msg = agent.Receive ()
+                return! loop (execute state msg)
+            }
+        loop initial)
+        
+    member this.Start () =
+        mailbox.Start ()
+
+    member this.Post msg =
+        mailbox.Post msg
+    
+    member this.PostAndReply<'Reply> msg : 'Reply =
+        mailbox.PostAndReply msg
+        
+    member this.TryPostAndReply<'Reply> msg timeout : Option<'Reply> =
+        mailbox.TryPostAndReply (msg, timeout)
+        
+    static member StartNew initial execute =
+        let agent = SimpleAgent (initial, execute)
+        agent.Start ()
+        agent
+
+
+type BootstrapMessage =
+    | GetClickCount of AsyncReplyChannel<int>
+    | SetClickCount of int
+
+type Bootstrap =
+    {
+        ClickCount : int;
+    }  
+
 type BootstrapViewModel () as this = 
     inherit MvxViewModel () 
+    
+    let agent = SimpleAgent.StartNew (Unchecked.defaultof<Bootstrap>) (fun state msg ->
+        match msg with
+        
+        | GetClickCount channel ->
+            channel.Reply state.ClickCount
+            state
 
-    let mutable clickCount = 0
+        | SetClickCount clickCount ->
+            { state with ClickCount = clickCount }
+    )
+
     member this.ClickCount
-        with get () = clickCount
+        with get () = agent.PostAndReply (fun x -> GetClickCount x)
         and private set value =
-            clickCount <- value
+            agent.Post (SetClickCount value)
             this.RaisePropertyChanged ("ClickCount")
             
 
-    member __.ClickText
+    member this.ClickText
         with get () =
             match this.ClickCount with
             | 0 -> "MvvmCross Button"
-            | x -> sprintf "Clicked %i times!" this.ClickCount
+            | x -> sprintf "Clicked %i times!" x
             
            
     member this.Click () =
