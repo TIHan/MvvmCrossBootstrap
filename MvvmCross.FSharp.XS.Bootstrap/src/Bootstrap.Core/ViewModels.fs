85e17e48cb30879087e1cbe855040c6fe0a11565
TIHan <lol.tihan@gmail.com>
2013-08-22 16:44:55 -0500

    Some F# work on BootstrapViewModel.

--- MvvmCross.FSharp.XS.Bootstrap/src/Bootstrap.Core/ViewModels.fs
+++ MvvmCross.FSharp.XS.Bootstrap/src/Bootstrap.Core/ViewModels.fs
@@ -1,7 +1,32 @@
 namespace Bootstrap.Core
 
 open System
+open System.Collections.ObjectModel
 open Cirrious.MvvmCross.ViewModels
 
-type BootstrapViewModel () = 
+type BootstrapViewModel () as this = 
     inherit MvxViewModel () 
+
+    let mutable clickCount = 0
+    member this.ClickCount
+        with get () = clickCount
+        and private set value =
+            clickCount <- value
+            this.RaisePropertyChanged ("ClickCount")
+            
+
+    member __.ClickText
+        with get () =
+            match this.ClickCount with
+            | 0 -> "MvvmCross Button"
+            | x -> sprintf "Clicked %i times!" this.ClickCount
+            
+           
+    member this.Click () =
+        this.ClickCount <- this.ClickCount + 1
+        
+    member this.ClickCommand
+        with get () = MvxCommand (fun () -> this.Click ())
+        
+    
+            
